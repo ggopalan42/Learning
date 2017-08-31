f6c66f87472317802d46e1ebf9697be2aa26dbf4
Giri Gopalan <ggopalan@yahoo.com>
2017-08-31 06:12:25 -0700

    Some restructuring of code

--- kaggle/zillow/z_features_select.py
+++ kaggle/zillow/z_features_select.py
@@ -7,9 +7,12 @@ Date: 8/11/2017
 
 '''
 
-import pandas as pd
-import yaml
 import os
+import yaml
+import sys
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
 
 yaml_file = 'features_select.yml'
 merged_2016_pkl = 'merged_2016_v2.pkl'
@@ -33,18 +36,26 @@ def split_df(fn, full_df):
         # Start subsetting the df
         sub_df = full_df[flist]
 
-        # Now store the subsetted df
-        sub_fn = os.path.join(cwd, fname+'.pkl')
-        print('Saving sub-setted df with name: {} to file {}'.format(fname, sub_fn))
-        sub_df.to_pickle(sub_fn)
+        # Split the df into train and test sets
+        split_ratio = fdict['train_test_split']['split_ratio']
+        random_state = fdict['train_test_split']['random_state']
+        train_df, test_df = train_test_split(sub_df, test_size=split_ratio, random_state=random_state)
+
+        # Now store the subsetted df, with train and test split
+        train_fn = os.path.join(cwd, fname+'_train.pkl')
+        test_fn = os.path.join(cwd, fname+'_test.pkl')
+        print('Saving sub-setted df with name: {} to files {} and {}'.format(fname, train_fn, test_fn))
+        print('Size of train set = {} and size of test set = {}'.format(len(train_df), len(test_df)))
+        train_df.to_pickle(train_fn)
+        test_df.to_pickle(test_fn)
 
 
 def trans_onehot(df, feature, trans_type):
-    print('In one hot with feature: {} and trans_type {}'.format(feature, trans_type))
+    print('In one hot with feature: {} and trans_type {}'.format(feature, trans_type['onehot']))
 
 
 def trans_normalize(df, feature, trans_type):
-    print('In normalize with feature: {} and trans_type {}'.format(feature, trans_type))
+    print('In normalize with feature: {} and trans_type {}'.format(feature, trans_type['normalize']))
 
 
 transform_mappings = {
@@ -53,12 +64,12 @@ transform_mappings = {
 }
 
 def transform_feature(df, feature, trans_type):
-    print('Transforming feature: {} to transform type: {}'.format(feature, trans_type))
+    print('Transforming feature: {}'.format(feature))
     tt_key = list(trans_type.keys())[0]    # Not sure if there is a simpler way to do this
     transform_mappings[tt_key](df, feature, trans_type)
 
 
-def features_transform(yaml_fn):
+def features_transform(yaml_fn, train_test):
     ''' This function will read in many dataframes as specified in the yaml file.
         and transform each feature of the df as specified '''
     # Read in the yaml file
@@ -66,16 +77,19 @@ def features_transform(yaml_fn):
         trans_cfg = yaml.load(yfh)
 
     # Now go through each yaml config and read in the pickled df
-    for fname, fdict in trans_cfg.items():
-        print('Reading DataFrame pickled in: {}'.format(fname+'.pkl'))
-        sub_df = pd.read_pickle(fname+'.pkl')
+    for base_fn, fdict in trans_cfg.items():
+        pkl_fn = '{}_{}{}'.format(base_fn, train_test, '.pkl')
+        print('Reading DataFrame pickled in: {}'.format(pkl_fn))
+        sub_df = pd.read_pickle(pkl_fn)
+        print('Size of df in file name {} is {}'.format(pkl_fn, len(sub_df)))
         # Now go through each feature and transform it
         for feature, trans_type in fdict['transform'].items():
-
             transform_feature(sub_df, feature, trans_type)
 
 if __name__ == '__main__':
     zdf = open_merged(merged_2016_pkl)
-    # split_df(yaml_file, zdf)
-    features_transform(yaml_file)
+    split_df(yaml_file, zdf)
+
+    # Transform the train sets
+    features_transform(yaml_file, train_test='train')
 
